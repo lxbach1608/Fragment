8c22ebc4166013b91c9d40b26ac494c7b9548975
bale <lxbach1608@gmail.com>
2023-02-20 02:39:23 +0700

    Fragment Manager full commit

---
+++ app/src/main/java/com/example/fragment/FirstFragment.java
@@ -0,0 +1,21 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FirstFragment extends Fragment {
+    TextView name;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        name = (TextView)view.findViewById(R.id.tvFirstFragment);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/fragment/SecondFragment.java
@@ -0,0 +1,21 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class SecondFragment extends Fragment {
+
+    TextView name;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        name = (TextView)view.findViewById(R.id.tvSecondFragment);
+        return view;
+    }
+}
