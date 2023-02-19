48e38217f28c38222a7bbd4f7010d2b7cf9ca0ab
bale <lxbach1608@gmail.com>
2023-02-20 02:34:00 +0700

    Fragment Manager commit

--- app/src/main/java/com/example/fragment/MainActivity.java
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.fragment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frlFragment, FirstFragment.class, null)
+                .commit();
+
+        Button btnFragment1 = (Button) findViewById(R.id.btnFragment1);
+        Button btnFragment2 = (Button) findViewById(R.id.btnFragment2);
+
+        btnFragment1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frlFragment, FirstFragment.class, null)
+                        .commit();
+            }
+        });
+
+        btnFragment2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frlFragment, SecondFragment.class, null)
+                        .commit();
+            }
+        });
+
+
+
+
     }
 }
