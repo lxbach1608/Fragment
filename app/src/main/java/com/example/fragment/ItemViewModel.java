61d3cd9d498ba3e070ea0a107f6e0bad5fd7c3e3
bale <lxbach1608@gmail.com>
2023-02-20 13:55:21 +0700

    Fragment Communicate commit

---
+++ app/src/main/java/com/example/fragment/ItemViewModel.java
@@ -0,0 +1,20 @@
+package com.example.fragment;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ItemViewModel extends ViewModel {
+
+    private final MutableLiveData<String> selectedItem = new MutableLiveData<String>();
+
+    public  void setData(String item){
+
+        selectedItem.setValue(item);
+
+    }
+
+    public LiveData<String> getSelectedItem(){
+        return  selectedItem;
+    }
+}
--- app/src/main/java/com/example/fragment/MainActivity.java
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -1,46 +1,44 @@
 package com.example.fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button firstFragmentBtn = findViewById(R.id.fragment1btn);
+    Button secondFragmentBtn = findViewById(R.id.fragment2btn);
+
+    TextView fragmentText = (TextView) findViewById(R.id.fragmentText);
+
+    private ItemViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.frlFragment, FirstFragment.class, null)
-                .commit();
+        viewModel = new ViewModelProvider(this).get(ItemViewModel.class);
 
-        Button btnFragment1 = (Button) findViewById(R.id.btnFragment1);
-        Button btnFragment2 = (Button) findViewById(R.id.btnFragment2);
+        viewModel.getSelectedItem().observe(this, item -> {
 
-        btnFragment1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.frlFragment, FirstFragment.class, null)
-                        .commit();
-            }
-        });
-
-        btnFragment2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.frlFragment, SecondFragment.class, null)
-                        .commit();
-            }
+            fragmentText.setText((item));
         });
 
 
+        firstFragmentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-
+            }
+        });
     }
+
 }
